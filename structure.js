087a31c85cd2bc7d5ddf3fe3c857f15684ef5d34
s9001848 <yenwu@ucdavis.edu>
2022-07-26 22:45:28 -0700

    adding DOM.js

--- structure.js
+++ structure.js
@@ -3,22 +3,22 @@ const ctx1 = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
 
-const canvas = document.getElementById('canvas2');
+const canvas2 = document.getElementById('canvas2');
 const ctx2 = canvas.getContext('2d');
 canvas2.width = 600;
 canvas2.height = 600;
 
-const canvas = document.getElementById('canvas3');
+const canvas3 = document.getElementById('canvas3');
 const ctx3 = canvas.getContext('2d');
 canvas3.width = 600;
 canvas3.height = 600;
 
-const canvas = document.getElementById('canvas4');
+const canvas4 = document.getElementById('canvas4');
 const ctx4 = canvas.getContext('2d');
 canvas4.width = 600;
 canvas4.height = 600;
 
-const canvas = document.getElementById('canvas5');
+const canvas5 = document.getElementById('canvas5');
 const ctx5 = canvas.getContext('2d');
 canvas5.width = 600;
 canvas5.height = 600;
